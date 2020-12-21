a5f41441542bed30fb8dd7526dea938d1baee59a
Prashant Singh <prashantsingh2408@gmail.com>
2020-12-21 23:30:08 +0530

    update db

--- user_app/config.php
+++ user_app/config.php
@@ -1,9 +1,15 @@
 <?php
 // Credentials
-$hostname = "localhost";
-$username = "root";     
-$password = "";
-$database = "fitness_app";
+
+$servername = "sql12.freemysqlhosting.net";
+$username = "sql12382803";
+$password = "AIgKGzqz36";
+$db = "sql12382803"; 
+
+// $hostname = "localhost";
+// $username = "root";     
+// $password = "";
+// $database = "fitness_app";
 
 
 //Establish Connection
